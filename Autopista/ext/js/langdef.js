221cb65f0d7aab16a3f988a95bfdcbee2ed351f4
ca-y-ro <caivro@hotmail.es>
2026-09-24 01:41:14 +0200

    Add files via upload

---
+++ Autopista/ext/js/langdef.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////
+//
+// Porta text resources
+//
+// Language (in English) : Spanish
+//
+// ISO 3166 country code : ES
+//
+// Translation by        : David Delgado Gómez
+//
+////////////////////////////////////////////////
+var ls_previous   = 'Anterior';
+var ls_next       = 'Siguiente';
+var ls_home       = 'Principal';
+var ls_showall    = 'Mostrar Todo';
+var ls_lightbox   = 'Diapositivas';
+var ls_albumview  = 'Vista en Album';
